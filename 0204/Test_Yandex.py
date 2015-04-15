b67a889b6e3c748065e2d61ac07b3e487c72733d
Денис Давыдов <davydov-den-nstu@yandex.ru>
2015-04-15 19:50:57 +0600

    Другой ассерт и получение атрибута
    Добавил другой ассерт для проверки вложенности строк
    Добавил тест с получение атрибута

--- 0204/Test_Yandex.py
+++ 0204/Test_Yandex.py
@@ -17,16 +17,9 @@ class Test_Yandex(TestCase):
         element = driver.find_element_by_xpath("//button[@type='submit']")
         element.click()
         title_value = driver.title
-        
-        element = driver.find_element_by_class_name('input__control')
-        input_text2 ='rjynhjkmyfz'
-        element.clear()
-        element.send_keys(input_text2)
-        element.submit()
-        
+
         try:
-            self.assertEqual(1, input_text in title_value, "Неверное значение")
-            self.assertEqual(1, "контрольная" in driver.title, "Неверное изменение раскладки")
+            self.assertIn(input_text,title_value, "Неверное значение")
         finally:
             driver.close()
 
@@ -45,7 +38,19 @@ class Test_Yandex(TestCase):
             self.assertEqual("Яндекс: задан пустой поисковый запрос", title_value, "Неверное значение")
         finally:
             driver.close()
+            
+    def test_search3(self):
+        driver = webdriver.Firefox()
+        driver.implicitly_wait(10)
+        driver.get("http://www.yandex.ru")
+        driver.find_element_by_xpath("//div[10]/div/a/span").click()
+        tag = driver.find_element_by_id("tab-rasp").get_attribute('href')
+        driver.get(tag)
+        try:
+            self.assertEqual(driver.current_url,tag,"Неправильный переход")
 
+        finally:
+            driver.close()
 
 if __name__ == '__main__':
     unittest.main()
